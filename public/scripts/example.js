90e8d4fcb3db94494446c1b7ce6616f45c2d6b7e
BrianNewsom <brian.newsom@colorado.edu>
2015-03-09 17:18:45 -0600

    completed milestone 3

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -39,11 +39,24 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var author = this.refs.author.getDOMNode().value.trim();
+        var text = this.refs.text.getDOMNode().value.trim();
+        if (!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({author: author, text: text});
+        this.refs.author.getDOMNode().value = '';
+        this.refs.text.getDOMNode().value = '';
+    },
     render: function() {
         return (
-        <div className="commentForm">
-            Hello World! I am a CommentForm.
-        </div>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+            <input type="text" placeholder="Your name" ref="author" />
+            <input type="text" placeholder="Say something..." ref="text" />
+            <input type="submit" value="Post" />
+            </form>
         );
     }
 });
@@ -68,13 +81,27 @@ var CommentBox = React.createClass({
         this.loadCommentsFromServer();
         setInterval(this.loadCommentsFromServer, this.props.pollInterval);
     },
+    handleCommentSubmit: function(comment) {
+            $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+    },
 
   render: function() {
     return (
       <div className="commentBox">
         Hello, world! I am a CommentBox.
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
